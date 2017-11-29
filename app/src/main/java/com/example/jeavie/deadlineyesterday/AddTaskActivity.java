3d5bbaac462bfdd1329dca59981344b241e0a180
Jeavie <dejeaview@gmail.com>
2017-11-30 00:31:44 +0300

    Date- and timepicker dialogs

--- app/src/main/java/com/example/jeavie/deadlineyesterday/AddTaskActivity.java
+++ app/src/main/java/com/example/jeavie/deadlineyesterday/AddTaskActivity.java
@@ -1,15 +1,34 @@
 package com.example.jeavie.deadlineyesterday;
 
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.text.format.DateUtils;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 import com.wafflecopter.charcounttextview.CharCountTextView;
 
 public class AddTaskActivity extends AppCompatActivity {
+
+    Calendar currentDate;
+    Calendar currentTime;
+    TextView setDate;
+    TextView setTime;
+    int minute, hour, year, month, day;
+    String format;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +46,69 @@ public class AddTaskActivity extends AppCompatActivity {
                 //stop?
             }
         });
+
+        setDate = (TextView)findViewById(R.id.setDate);
+
+        currentDate = Calendar.getInstance();
+        year = currentDate.get(Calendar.YEAR);
+        month = currentDate.get(Calendar.MONTH) + 1;
+        day = currentDate.get(Calendar.DAY_OF_MONTH);
+        setDate.setText(day + "/" + month + "/" + year);
+
+        setDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(AddTaskActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        month=month+1;
+                        setDate.setText(dayOfMonth + "/" + month + "/" + year);
+                    }
+                },year, month, day);
+                datePickerDialog.show();
+            }
+        });
+
+        setTime = (TextView)findViewById(R.id.setTime);
+
+        currentTime = Calendar.getInstance();
+        hour = currentTime.get(Calendar.HOUR_OF_DAY);
+        minute = currentTime.get(Calendar.MINUTE);
+        selectedTimeFormat(hour);
+        setTime.setText(hour + " : " + minute + " " + format);
+
+        setTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(AddTaskActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        selectedTimeFormat(hourOfDay);
+                        setTime.setText(hourOfDay + " : " + minute + " " + format);
+                    }
+                }, hour, minute, true);
+                timePickerDialog.show();
+            }
+        });
+
+
     }
+
+    public void selectedTimeFormat(int hour){
+        if (hour == 0){
+            hour += 12;
+            format = "AM";
+        } else if (hour == 12){
+            format = "PM";
+        } else if (hour > 12){
+            hour -= 12;
+            format = "PM";
+        } else {
+            format = "AM";
+        }
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
