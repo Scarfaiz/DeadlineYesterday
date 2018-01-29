2368436fa296b20e21badb294f98f5a3adebf3eb
Jeavie <dejeaview@gmail.com>
2018-01-29 20:07:26 +0300

    Deleted extra things

--- app/src/main/java/com/example/jeavie/deadlineyesterday/WeekActivity.java
+++ app/src/main/java/com/example/jeavie/deadlineyesterday/WeekActivity.java
@@ -68,57 +68,15 @@ public class WeekActivity extends AppCompatActivity implements CalendarPrepareCa
                 onBackPressed();
             }
         });
-        //events.add(new EventModel());
 
         listCalendar = findViewById(R.id.listCalendar);
 
         ListCalendarConfiguration.Builder listBuilder = new ListCalendarConfiguration.Builder();
 
-        EventDecoratorFactory eventDecoratorFactory = new EventDecoratorFactory() {
-            @Override
-            public EventDecorator createEventDecorator(View parent) {
-                return new EventDecoratorImpl(parent);
-            }
-        };
-
-        WeekDecoratorFactory weekDecoratorFactory = new WeekDecoratorFactory() {
-            @Override
-            public WeekDecorator createWeekDecorator(View parent) {
-                return new WeeDecoratorImpl(parent);
-            }
-        };
-
-        MonthDecoratorFactory monthDecoratorFactory = new MonthDecoratorFactory() {
-            @Override
-            public MonthDecorator createMonthDecorator(View parent) {
-                return new MonthDecoratorImpl(parent);
-            }
-        };
-
         listBuilder.setDisplayPeriod(Calendar.MONTH, 3);
-//        listBuilder.setEventLayout(R.layout.custom_event_layout, eventDecoratorFactory);
-//        listBuilder.setWeekLayout(R.layout.custom_week_title_layout, weekDecoratorFactory);
-//        listBuilder.setMonthLayout(R.layout.custom_month_calendar_event_layout, monthDecoratorFactory);
-//        listBuilder.setEventsProcessor(new CustomEventProcessor());
 
         listCalendar.setCalendarPrepareCallback(this);
         listCalendar.prepareCalendar(listBuilder.build());
-        listCalendar.setOnEventClickListener(new OnEventClickListener() {
-            @Override
-            public void onEventClick(Event event, int position) {
-                Log.i("onEventClick", String.valueOf(event));
-            }
-
-            @Override
-            public void onSyncClick(Event event, int position) {
-                Log.i("onSyncClick", String.valueOf(event));
-            }
-
-            @Override
-            public void onEventLongClick(Event event, int position) {
-                Log.i("onEventLongClick", String.valueOf(event));
-            }
-        });
 
         FloatingActionButton addTask=findViewById(R.id.addTask);
         addTask.setOnClickListener(new View.OnClickListener() {
@@ -152,134 +110,6 @@ public class WeekActivity extends AppCompatActivity implements CalendarPrepareCa
         return super.onOptionsItemSelected(item);
     }
 
-    private class MonthDecoratorImpl implements MonthDecorator {
-
-        private ImageView monthBackground;
-        private TextView monthTitle;
-        private Custom custom;
-
-        public MonthDecoratorImpl(View parent) {
-            monthBackground = parent.findViewById(R.id.month_background);
-            monthTitle = parent.findViewById(R.id.month_label);
-        }
-
-        @Override
-        public void onBindMonthView(View view, Calendar month) {
-            monthBackground.setImageDrawable(null);
-
-            final int backgroundId = getBackgroundId(month.get(Calendar.MONTH));
-            monthTitle.setText(month.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault()));
-//            Picasso.with(monthTitle.getContext().getApplicationContext()).load(backgroundId).into(monthBackground, new Callback() {
-//                @Override
-//                public void onSuccess() {
-//                    if (Build.VERSION.SDK_INT > 13) {
-//                        monthBackground.setScrollX(0);
-//                        monthBackground.setScrollY(0);
-//                    }
-//                }
-//
-//                @Override
-//                public void onError() {
-//
-//                }
-//            });
-        }
-
-        @NonNull
-        @Override
-        public RecyclerView.OnScrollListener getScrollListener() {
-            custom = new Custom();
-            return custom;
-        }
-    }
-
-    private class EventDecoratorImpl implements EventDecorator {
-
-        private TextView textView;
-
-        public EventDecoratorImpl(View parent) {
-            textView = parent.findViewById(R.id.day_title);
-        }
-
-        @Override
-        public void onBindEventView(View view, Event event, ListItemModel previous, int position) {
-            view.setBackgroundColor(ContextCompat.getColor(WeekActivity.this, R.color.light_grey));
-            textView.setText(event.getEventTitle() + "\n" + event.getEventStartDate());
-        }
-    }
-
-    private class WeeDecoratorImpl implements WeekDecorator {
-
-        private TextView title;
-
-        public WeeDecoratorImpl(View parent) {
-            title = parent.findViewById(io.github.memfis19.cadar.R.id.week_title);
-        }
-
-        @Override
-        public void onBindWeekView(View view, Pair<Calendar, Calendar> period) {
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append(title.getContext().getString(io.github.memfis19.cadar.R.string.calendar_week));
-            stringBuilder.append("custom ");
-            stringBuilder.append(period.first.get(Calendar.WEEK_OF_YEAR));
-            stringBuilder.append(", ");
-            stringBuilder.append(DateFormat.format("dd MMM", period.first));
-            stringBuilder.append(" - ");
-            stringBuilder.append(DateFormat.format("dd MMM", period.second));
-
-            final Spannable date = new SpannableString(stringBuilder.toString());
-
-            title.setText(date);
-        }
-    }
-
-    private int getBackgroundId(int month) {
-        int backgroundId = io.github.memfis19.cadar.R.drawable.bkg_12_december;
-
-        if (month == Calendar.JANUARY) {
-            backgroundId = io.github.memfis19.cadar.R.drawable.bkg_01_january;
-        } else if (month == Calendar.FEBRUARY) {
-            backgroundId = io.github.memfis19.cadar.R.drawable.bkg_02_february;
-        } else if (month == Calendar.MARCH) {
-            backgroundId = io.github.memfis19.cadar.R.drawable.bkg_03_march;
-        } else if (month == Calendar.APRIL) {
-            backgroundId = io.github.memfis19.cadar.R.drawable.bkg_04_april;
-        } else if (month == Calendar.MAY) {
-            backgroundId = io.github.memfis19.cadar.R.drawable.bkg_05_may;
-        } else if (month == Calendar.JUNE) {
-            backgroundId = io.github.memfis19.cadar.R.drawable.bkg_06_june;
-        } else if (month == Calendar.JULY) {
-            backgroundId = io.github.memfis19.cadar.R.drawable.bkg_07_july;
-        } else if (month == Calendar.AUGUST) {
-            backgroundId = io.github.memfis19.cadar.R.drawable.bkg_08_august;
-        } else if (month == Calendar.SEPTEMBER) {
-            backgroundId = io.github.memfis19.cadar.R.drawable.bkg_09_september;
-        } else if (month == Calendar.OCTOBER) {
-            backgroundId = io.github.memfis19.cadar.R.drawable.bkg_10_october;
-        } else if (month == Calendar.NOVEMBER) {
-            backgroundId = io.github.memfis19.cadar.R.drawable.bkg_11_november;
-        }
-
-        return backgroundId;
-    } //background
-
-    private class Custom extends RecyclerView.OnScrollListener {
-
-        private View monthBackground;
-
-        Custom() {
-        }
-
-        public void setMonthBackground(View monthBackground) {
-            this.monthBackground = monthBackground;
-        }
-
-        @Override
-        public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-            super.onScrolled(recyclerView, dx, dy);
-            if (monthBackground != null) monthBackground.scrollBy(dx, (-1) * (dy / 10));
-        }
-    }
 
     @Override
     public void onCalendarReady(CalendarController calendar) {
@@ -295,28 +125,6 @@ public class WeekActivity extends AppCompatActivity implements CalendarPrepareCa
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
         listCalendar.releaseCalendar();
     }
-
-    class CustomEventProcessor extends EventsProcessor<Pair<Calendar, Calendar>, List<Event>> {
-
-        public CustomEventProcessor() {
-            super(false, null, true);
-        }
-
-        @Override
-        protected void processEventsAsync(final Pair<Calendar, Calendar> target, final EventsProcessorCallback<Pair<Calendar, Calendar>, List<Event>> eventsProcessorCallback) {
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    for (int i = 0; i < 5; ++i) {
-                        //events.add(new EventModel());
-                    }
-                    eventsProcessorCallback.onEventsProcessed(target, events);
-                }
-            }, 3000);
-        }
-    }
-
 }
